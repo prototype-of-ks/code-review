41e03bb09c274f615fbca838a17b5aa7ae3b069e
prototype-of-ks <2284720956@qq.com>
2023-11-04 15:47:15 +0800

    Nov 3/Git actions PUSH/Comments on PR 2

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-const githubToken = process.env.GITHUB_TOKEN;
-const githubRepository = process.env.GITHUB_REPOSITORY;
-const githubSHA = process.env.GITHUB_SHA;
-const githubRef = process.env.GITHUB_REF;
-const githubEventPath = process.env.GITHUB_EVENT_PATH;
+// const githubToken = process.env.GITHUB_TOKEN;
+// const githubRepository = process.env.GITHUB_REPOSITORY;
+// const githubSHA = process.env.GITHUB_SHA;
+// const githubRef = process.env.GITHUB_REF;
+// const githubEventPath = process.env.GITHUB_EVENT_PATH;
 
-console.log(`GITHUB_TOKEN: ${githubToken}`);
-console.log(`GITHUB_REPOSITORY: ${githubRepository}`);
-console.log(`GITHUB_SHA: ${githubSHA}`);
-console.log(`GITHUB_REF: ${githubRef}`);
-console.log(`GITHUB_EVENT_PATH: ${githubEventPath}`);
+import { commentsOnPR } from './common/ci/github/commentsOnPR';
+
+// console.log(`GITHUB_TOKEN: ${githubToken}`);
+// console.log(`GITHUB_REPOSITORY: ${githubRepository}`);
+// console.log(`GITHUB_SHA: ${githubSHA}`);
+// console.log(`GITHUB_REF: ${githubRef}`);
+// console.log(`GITHUB_EVENT_PATH: ${githubEventPath}`);
 
 export const main = async () => {
-    
+  commentsOnPR('This is default comments');
 };
 
 main().catch(() => {
-    process.exit(1);
-})
+  process.exit(1);
+});
